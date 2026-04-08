565f7f8fe25823c4e965136f07b60fdc0e3857fa
VSClarity <trashkov.r2007@gmail.com>
2026-04-08 22:26:32 +0300

    Add variables tasks

---
+++ assets/variables.js
@@ -0,0 +1,36 @@
+// 1
+let a = 5;
+let b = 2;
+let multiply_ab = a * b;
+console.log(multiply_ab);
+
+// 2
+let c = 10;
+let d = 2;
+let division_cd = c / d;
+console.log(division_cd);
+
+// 3
+let e = 'abc';
+let f = 'def';
+let sum_ef = e + f;
+console.log(sum_ef);
+
+// 4
+let numValue = 11;
+let booleanValue = true;
+let textValue = "java script";
+let textValueNumber = "100";
+console.log(typeof numValue);
+console.log(typeof booleanValue);
+console.log(typeof textValue);
+console.log(typeof textValueNumber);
+
+// 5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
