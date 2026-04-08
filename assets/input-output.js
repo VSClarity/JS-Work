f5b1d1edb9c8cc13d5cda2fcaa78cf4c63acf14c
VSClarity <trashkov.r2007@gmail.com>
2026-04-08 22:29:04 +0300

    Add input/output tasks

---
+++ assets/input-output.js
@@ -0,0 +1,21 @@
+// 2.1
+let questionUser = Number(prompt("Введите число:"));
+let resultQuestion = questionUser ** 2;
+alert(resultQuestion);
+
+// 2.2
+let inputNumUser = Number(prompt("Введите первое число"));
+let inputNumUser_2 = Number(prompt("Введите второе число"));
+let outputNumUserSum = (numUser + numUser_2) / 2;
+alert(outputNumUserSum);
+
+// 2.3
+let min_sec_User = Number(prompt("Введите кол-во минут"));
+let min_sec_UserAnswer = min_sec_User * 60;
+alert(min_sec_UserAnswer);
+
+// 2.4
+let greeting = "Hello, ";
+let userName = prompt("Введите ваше имя")
+let greetingUserName = greeting + userName;
+alert(greetingUserName);
